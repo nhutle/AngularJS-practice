8ac7ed84fa5e320d47edbf3eb5f3bf9b3587e765
Nhut Le <nhutle@xentralmethods.com>
2016-09-12 19:11:32 +0800

    fetch tasks by date from controller

--- javascripts/controllers/home-controller.js
+++ javascripts/controllers/home-controller.js
@@ -1,105 +1,35 @@
-'use strict';
-
 (function(angular) {
+  'use strict';
+
   angular
     .module('frontendTestApp')
-    .controller('homeController', ['$scope',
-      function($scope) {
-        // calendario
-//         var $calendar = $('#calendar'),
-//           cal = $calendar.calendario({
-//             onDayClick: function($el, data, dateProperties) {
-//               $scope.$apply(function() {
-//                 $scope.selectedDate = [dateProperties.weekdayname, dateProperties.monthname, dateProperties.day].join(' ');
-//               });
-//             }
-//           });
-// ''
-//         $scope.selectedMonth = [cal.getMonthName(), cal.getYear()].join(' ');
-//         $scope.selectedDate = [cal.getWeekdayname(), cal.getMonthName(), cal.getDate()].join(' ');
+    .controller('homeController', [
+      '$scope',
+      '$location',
+      '$routeParams',
+      '$timeout',
+      '$log',
+      'homeService',
+      function($scope, $location, $routeParams, $timeout, $log, homeService) {
+        homeService.getTasksByDate($routeParams.dateId).then(function(data) {
+          console.log(data);
+          $scope.tasksByDate = data;
+        }, function(message) {
+          $log.error(message);
+        });
+
+        // http://stackoverflow.com/questions/24143945/location-path-updates-after-the-second-click
+        $scope.getTasksByDate = function(date) {
+          $timeout(function() {
+            $scope.currentPath = $location.path('/dates/' + date);
+          }, 0);
+        };
+
+        $scope.getTodayTasks = function() {
+          var dateId = new Date().toISOString().slice(0, 10);
+
+          $scope.getTasksByDate(dateId);
+        };
       }
     ]);
 })(angular);
-
-
-//////
-///
-///
-///
-///
-///////
-///
-// data: {
-//   date1: {
-//     user1: {
-//       user_id: nhutle
-//       username: Nhut Le
-//       avatar: nhutle_avatar.png
-//       task: {
-//           task_id: task1
-//           comment: add new comment to 8am post
-//           posts: {
-//             post1: {
-//               post_id: time
-//               createdTime: 8:00am
-//               content: {
-//                 post_child {
-//                   post_child_id
-//                   image: image_url,
-//                   title: Notes UK
-//                   source: twitter
-//                   status: approved
-//                 }
-//               }
-//             }
-//         }
-//       }
-//     }
-//   }
-// }
-// ///
-// ///
-// ///
-
-
-// data =[{
-//   {
-//     'date': '10-09-2016',
-//     'campaigns': [
-
-//     ]
-//   }
-// }];
-
-// campaigns:
-//   id
-//   name
-//   user_id
-//   photo
-//   content
-//   starting time
-//   comment
-//   status
-//   source
-
-
-// campaigns:
-
-
-
-
-
-
-
-
-
-// posts
-//   post_id
-//   source
-
-
-
-// user:
-//   id
-//   name
-//   avatar
