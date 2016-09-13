fa7e666ed1d934411af5af0dbb7aab4ff0ec228e
Nhut Le <nhutle@xentralmethods.com>
2016-09-14 07:05:21 +0800

    show selected date based on route url

--- javascripts/controllers/home-controller.js
+++ javascripts/controllers/home-controller.js
@@ -11,9 +11,17 @@
       '$log',
       'homeService',
       function($scope, $location, $routeParams, $timeout, $log, homeService) {
+        var date = new Date($routeParams.dateId);
+        $scope.da = date;
+
         homeService.getTasksByDate($routeParams.dateId).then(function(data) {
-          console.log(data);
+          var date = new Date($routeParams.dateId),
+            days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+          $scope.selectedDate = [days[date.getDay()], months[date.getMonth()], date.getFullYear()].join(' ');
           $scope.tasksByDate = data;
+
         }, function(message) {
           $log.error(message);
         });
@@ -21,7 +29,7 @@
         // http://stackoverflow.com/questions/24143945/location-path-updates-after-the-second-click
         $scope.getTasksByDate = function(date) {
           $timeout(function() {
-            $scope.currentPath = $location.path('/dates/' + date);
+            $location.path('/dates/' + date);
           }, 0);
         };
 
--- javascripts/directives/calendario-directive.js
+++ javascripts/directives/calendario-directive.js
@@ -16,24 +16,25 @@
             var cal = $(element).calendario({
               onDayClick: function($el, data, dateProperties) {
                 // highlight selected day
-                $('.fc-body').find('.selected').removeClass('selected');
-                $el.addClass('selected');
+                // $('.fc-body').find('.selected').removeClass('selected');
+                // $el.addClass('selected');
 
                 // pre-process date
                 var selectedDay = (dateProperties.day < 10 ? '0' : '') + dateProperties.day,
                   selectedMonth = (dateProperties.month < 10 ? '0' : '') + dateProperties.month,
                   selectedDate = [dateProperties.year, selectedMonth, selectedDay].join('-');
 
-                scope.$apply(function() {
-                  scope.selectedDate = [dateProperties.weekdayname, dateProperties.monthname, dateProperties.day].join(' ');
-                });
-
                 scope.getTasksByDate({ date: selectedDate });
+
+                // scope.$apply(function() {
+                //   scope.selectedDate = [dateProperties.weekdayname, dateProperties.monthname, dateProperties.day].join(' ');
+                // });
+
               }
             });
 
-            scope.selectedMonth = [cal.getMonthName(), cal.getYear()].join(' ');
-            scope.selectedDate = [cal.getWeekdayname(), cal.getMonthName(), cal.getDate()].join(' ');
+            // scope.selectedMonth = [cal.getMonthName(), cal.getYear()].join(' ');
+            // scope.selectedDate = [cal.getWeekdayname(), cal.getMonthName(), cal.getDate()].join(' ');
           },
         };
       }
